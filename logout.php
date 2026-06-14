104cb5bef74aec2988887f0426d5fc9ef71323c3
fathir-flop <jollyroger7765@gmail.com>
2026-06-14 14:17:11 +0800

    logout session admin/super admin

--- logout.php
+++ logout.php
@@ -1,7 +1,13 @@
 <?php
 session_start();
 
-session_destroy();
-header("Location: login.php");
-exit;
+if (isset($_SESSION['role']) && $_SESSION['role'] === 'admin') {
+    session_destroy();
+    header("Location: login_admin.php");
+    exit;
+} else {
+    session_destroy(); 
+    header("Location: login.php");
+    exit;
+}
 ?>
