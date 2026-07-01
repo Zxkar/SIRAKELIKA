fb848a9c313d75f56c67e4e2bc8a83b35a9a7654
fathir-flop <jollyroger7765@gmail.com>
2026-07-01 18:12:01 +0800

    registrasi copy dari github karena yg kemarin tersave yg se ubah

--- auth/register.php
+++ auth/register.php
@@ -37,7 +37,7 @@ if(isset($_POST['register'])){
         $new_user_id = mysqli_insert_id($conn);
         
         $_SESSION['user_logged_in'] = true;
-        $_SESSION['user_id']        = $new_user_id;
+        $_SESSION['id_user']        = $new_user_id;
         $_SESSION['username']       = $nama;
         $_SESSION['role']           = 'mahasiswa';
         
@@ -162,4 +162,3 @@ document.getElementById('cpw').addEventListener('input', function(){
 </script>
 </body>
 </html>
-
