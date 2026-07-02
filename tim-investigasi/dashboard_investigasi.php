0f7487a673bd47c2ad4201e9a1e7994b75f96e0c
Zxkar <162864689+Zxkar@users.noreply.github.com>
2026-07-02 11:56:28 +0800

    Update dashboard_investigasi.php

--- tim-investigasi/dashboard_investigasi.php
+++ tim-investigasi/dashboard_investigasi.php
@@ -1,5 +1,10 @@
 <?php
 session_start();
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+header("Expires: Sat, 01 Jan 2000 00:00:00 GMT");
+
 include '../config/conn.php';
 
 if(!isset($_SESSION['username']) || $_SESSION['role'] !== 'investigasi'){
